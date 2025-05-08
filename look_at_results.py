918208b0d61b2f55a679d398d582ef70ab98b11c
Luke Wismer <lukewismer77@gmail.com>
2025-05-08 15:35:30 -0600

    Final Update

--- look_at_results.py
+++ look_at_results.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv("players_with_stats_with_preds.csv")
+df = pd.read_csv("data_with_metrics.csv")
 
-hitters = df[(df["is_hitter"] == False) & (df["old_rank"] >= 60)]
+df = df[df["old_rank"] > 64]
 
-top_10_hitters = hitters\
-    .sort_values(by="mu_pitcher", ascending=True)\
-    [["name", "old_rank", "mu_pitcher", "low95_pitcher", "high95_pitcher"]]
+df['profit_pct'] = df['pred_profit'] / df['buy_price'] * 100
+
+top_10_hitters = (
+    df
+    .sort_values(by="profit_pct", ascending=False)
+    [["name","pred_profit","buy_price","pred_quick_sell_value","profit_pct","mu_pitcher","mu_hitter"]]
+)
 
 print(top_10_hitters.head(20))
